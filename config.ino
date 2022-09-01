94aaeb6111fc8c33506e0c9c262eff4566cb7b9d
Mahdi Hajian <mahdi.haj.1375@gmail.com>
2022-09-01 13:12:32 +0430

    init

---
+++ config.ino
@@ -0,0 +1,18 @@
+int blue = 3;
+int red = 4;
+int green = 5;
+void setup() {
+  pinMode(blue, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+
+void loop() {
+  setupRgb(500,500,500);
+}
+
+void setupRgb(int b, int r, int g){
+  analogWrite(blue,b);
+  analogWrite(red,r);
+  analogWrite(green,g);
+}
