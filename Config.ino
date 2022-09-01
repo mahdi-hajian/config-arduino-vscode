0c7550760a5c4c8e14ebe0b3ebeef01199cd31b8
Mahdi Hajian <mahdi.haj.1375@gmail.com>
2022-09-01 15:59:13 +0430

    edit

--- Config.ino
+++ Config.ino
@@ -1,10 +1,11 @@
+
 int blue = 3;
 int red = 5;
 int green = 6;
 int red2 = 9;
 int green2 = 10;
 int blue2 = 11;
-int delayTime = 70;
+int delayTime = 50;
 void setup() {
   pinMode(blue, OUTPUT);
   pinMode(red, OUTPUT);
@@ -16,11 +17,11 @@ void setup() {
 
 void loop() {
   setupRgb(0,0,1023);
-  delay(delayTime);
-  setupRgb(1023,0,0);
-  delay(delayTime);
-  setupRgb(0,1023,0);
-  delay(delayTime);
+  // delay(delayTime);
+  // setupRgb(1023,0,0);
+  // delay(delayTime);
+  // setupRgb(0,1023,0);
+  // delay(delayTime);
 }
 
 void setupRgb(int b, int r, int g){
